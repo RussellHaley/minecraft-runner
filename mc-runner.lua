2bcf62be22ebdf7e66aa1e74e4782dc121dc575e
russellhaley <russ.haley@gmail.com>
2018-09-17 22:42:26 -0700

    Added static content server. Can server a dir, but only downloads content.

--- mc-runner.lua
+++ mc-runner.lua
@@ -3,6 +3,12 @@ local request = require "http.request"
 local http_server = require "http.server"
 local websocket = require "http.websocket"
 local http_headers = require "http.headers"
+local new_headers = require "http.headers".new
+local http_util = require "http.util"
+local http_version = require "http.version"
+local lpeg = require "lpeg"
+local uri_patts = require "lpeg_patterns.uri"
+local ce = require "cqueues.errno"
 local rolling_logger = require "logging.rolling_file"
 local dkjson = require "dkjson"
 local persist = require "persist"
@@ -18,7 +24,7 @@ uuid.randomseed(12365843213246849613)
 local req_timeout = 10
 local ShutDown = false
 local Sessions = {}
-
+local uri_reference = uri_patts.uri_reference * lpeg.P(-1)
 local conf = require('config')
 
 local logger = rolling_logger(conf.base_path .. "/" .. conf.debug_file_name, conf.file_roll_size or 1024*1024*10, conf.max_log_files or 31)
@@ -31,55 +37,55 @@ local connection_log = rolling_logger(conf.base_path .. "/" .. conf.connection_l
 
 local function check_mcs_version(uri)
 
-	local req = request.new_from_uri(uri)
+    local req = request.new_from_uri(uri)
 
-	local headers, stream = req:go(req_timeout)
-	if headers == nil then
-		io.stderr:write(tostring(stream), "\n")
-		os.exit(1)
-	end
+    local headers, stream = req:go(req_timeout)
+    if headers == nil then
+        io.stderr:write(tostring(stream), "\n")
+        os.exit(1)
+    end
 
-	local body, err = stream:get_body_as_string()
-	if not body and err then
-		io.stderr:write(tostring(err), "\n")
-		os.exit(1)
-	end
+    local body, err = stream:get_body_as_string()
+    if not body and err then
+        io.stderr:write(tostring(err), "\n")
+        os.exit(1)
+    end
 
-	
-	local dl_uri, jar = body:match(conf.pattern)
-	
-	if not dl_uri then
-		logger:error('Failed to parse uri filename')
-		return nil, 'No URI'
-	end
+    
+    local dl_uri, jar = body:match(conf.pattern)
+    
+    if not dl_uri then
+        logger:error('Failed to parse uri filename')
+        return nil, 'No URI'
+    end
 
-	if not jar then
-		logger:error('Failed to parse jar filename')
-		return nil, 'No jar file'
-	end
-	return dl_uri, jar
-	
+    if not jar then
+        logger:error('Failed to parse jar filename')
+        return nil, 'No jar file'
+    end
+    return dl_uri, jar
+    
 end
 
 local function download_file(uri, dest, name)
-	req = request.new_from_uri(uri)
-	headers, stream = req:go(req_timeout)
-	if headers then
-		for k,v in headers:each() do
-			print(k,v)
-		end
-		local name = dest..'/'.. name
-		print(name)
-		local file = io.open(name,'w')
-		local ok = stream:save_body_to_file(file)
-		if ok then
-			
-			return ok
-		end
-	else
-		--FREAKOUT
-		return nil, 'failed to get the file'
-	end
+    req = request.new_from_uri(uri)
+    headers, stream = req:go(req_timeout)
+    if headers then
+        for k,v in headers:each() do
+            print(k,v)
+        end
+        local name = dest..'/'.. name
+        print(name)
+        local file = io.open(name,'w')
+        local ok = stream:save_body_to_file(file)
+        if ok then
+            
+            return ok
+        end
+    else
+        --FREAKOUT
+        return nil, 'failed to get the file'
+    end
 
 end
 
@@ -92,25 +98,25 @@ repeat
     
   local uri, jar = check_mcs_version(conf.start_page)
   if not mcs_versions:item_exists(jar) then
-	  mcs_versions:add_item(jar, {jar = jar, uri = uri, timestamp = os.date("%Y-%m-%d_%H%M%S")}) 
-	  logger:info(string.format('Found new file: %s - %s', jar, uri))
-	  local f = function() 
-			  print(uri, conf.download_dir, jar)
-			  local ok, emsg = download_file(uri, conf.download_dir, jar)
-			  if ok  then
-				  logger:info("Download Complete.")
-			  else
-				  logger:error(emsg)
-			  end	
-		  end
-		  f()
+      mcs_versions:add_item(jar, {jar = jar, uri = uri, timestamp = os.date("%Y-%m-%d_%H%M%S")}) 
+      logger:info(string.format('Found new file: %s - %s', jar, uri))
+      local f = function() 
+              print(uri, conf.download_dir, jar)
+              local ok, emsg = download_file(uri, conf.download_dir, jar)
+              if ok  then
+                  logger:info("Download Complete.")
+              else
+                  logger:error(emsg)
+              end    
+          end
+          f()
   else
-  -- 			print('version exists')
-	  --~ do nothing right now
+  --             print('version exists')
+      --~ do nothing right now
   end
   cqueues.sleep(2)
   --NOTE: Count will never equal 1!
-  -- 		counter = counter + 1
+  --         counter = counter + 1
 until counter == 1
 end
 
@@ -119,7 +125,212 @@ local function write_to_process(str)
   print("sent: " .. str)
 end
 
-local function ProcessWebsocketMessage(t, msg)
+--- Get a UUID from the OS
+-- return: Returns a system generated UUID
+-- such as "4f1c1fbe-87a7-11e6-b146-0c54a518c15b"
+local function GetUUID()
+    local u = uuid()
+    print("here's a new uuid: ",u)
+    return u
+end
+
+
+local default_server = string.format("%s/%s", http_version.name, http_version.version)
+
+local xml_escape do
+	local escape_table = {
+		["'"] = "&apos;";
+		["\""] = "&quot;";
+		["<"] = "&lt;";
+		[">"] = "&gt;";
+		["&"] = "&amp;";
+	}
+	function xml_escape(str)
+		str = string.gsub(str, "['&<>\"]", escape_table)
+		str = string.gsub(str, "[%c\r\n]", function(c)
+			return string.format("&#x%x;", string.byte(c))
+		end)
+		return str
+	end
+end
+
+local human do -- Utility function to convert to a human readable number
+	local suffixes = {
+		[0] = "";
+		[1] = "K";
+		[2] = "M";
+		[3] = "G";
+		[4] = "T";
+		[5] = "P";
+	}
+	local log = math.log
+	if _VERSION:match("%d+%.?%d*") < "5.1" then
+		log = require "compat53.module".math.log
+	end
+	function human(n)
+		if n == 0 then return "0" end
+		local order = math.floor(log(n, 2) / 10)
+		if order > 5 then order = 5 end
+		n = math.ceil(n / 2^(order*10))
+		return string.format("%d%s", n, suffixes[order])
+	end
+end
+
+local function static_reply(myserver, stream, req_headers) -- luacheck: ignore 212
+
+	--~ HACK: NEED TO ADD ../ TO WEBSERVER PATH 
+	--~ because we cd into the minecraft directory in the Run() method
+	local up_a_dir = '../'
+    -- Read in headers
+    assert(req_headers)
+    local req_method = req_headers:get ":method"
+
+    -- Log request to stdout
+    assert(io.stdout:write(string.format('[%s] "%s %s HTTP/%g"  "%s" "%s"\n',
+        os.date("%d/%b/%Y:%H:%M:%S %z"),
+        req_method or "",
+        req_headers:get(":path") or "",
+        stream.connection.version,
+        req_headers:get("referer") or "-",
+        req_headers:get("user-agent") or "-"
+    )))
+
+    -- Build response headers
+    local res_headers = new_headers()
+    res_headers:append(":status", nil)
+    res_headers:append("server", default_server)
+    res_headers:append("date", http_util.imf_date())
+
+    if req_method ~= "GET" and req_method ~= "HEAD" then
+        res_headers:upsert(":status", "405")
+        assert(stream:write_headers(res_headers, true))
+        return
+    end
+
+    local path = req_headers:get(":path")
+    local uri_t = assert(uri_reference:match(path), "invalid path")
+    path = http_util.resolve_relative_path("/", uri_t.path)
+    local real_path = up_a_dir .. conf.static_dir .. path
+    print(path, real_path)
+    local file_type = lfs.attributes(real_path, "mode")
+    print(string.format("file type: %s", file_type)) 
+print('one')
+    if file_type == "directory" then
+    print('two')
+        -- directory listing
+        path = path:gsub("/+$", "") .. "/"
+        res_headers:upsert(":status", "200")
+        res_headers:append("content-type", "text/html; charset=utf-8")
+        assert(stream:write_headers(res_headers, req_method == "HEAD"))
+        if req_method ~= "HEAD" then
+        --~ content: "ð";
+            assert(stream:write_chunk(string.format([[
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index of %s</title>
+    <style>
+        a {
+            float: left;
+        }
+        a::before {
+            width: 1em;
+            float: left;
+            content: "\0000a0";
+        }
+        a.directory::before {
+            
+            content: "";
+        }
+        table {
+            width: 800px;
+        }
+        td {
+            padding: 0 5px;
+            white-space: nowrap;
+        }
+        td:nth-child(2) {
+            text-align: right;
+            width: 3em;
+        }
+        td:last-child {
+            width: 1px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Index of %s</h1>
+    <table>
+        <thead><tr>
+            <th>File Name</th><th>Size</th><th>Modified</th>
+        </tr></thead>
+        <tbody>
+]], xml_escape(path), xml_escape(path)), false))
+            -- lfs doesn't provide a way to get an errno for attempting to open a directory
+            -- See https://github.com/keplerproject/luafilesystem/issues/87
+            for filename in lfs.dir(real_path) do
+                if not (filename == ".." and path == "/") then -- Exclude parent directory entry listing from top level
+                    local stats = lfs.attributes(real_path .. "/" .. filename)
+                    if stats.mode == "directory" then
+                        filename = filename .. "/"
+                    end
+                    assert(stream:write_chunk(string.format("\t\t\t<tr><td><a class='%s' href='%s'>%s</a></td><td title='%d bytes'>%s</td><td><time>%s</time></td></tr>\n",
+                        xml_escape(stats.mode:gsub("%s", "-")),
+                        xml_escape(http_util.encodeURI(path .. filename)),
+                        xml_escape(filename),
+                        stats.size,
+                        xml_escape(human(stats.size)),
+                        xml_escape(os.date("!%Y-%m-%d %X", stats.modification))
+                    ), false))
+                end
+            end
+            assert(stream:write_chunk([[
+        </tbody>
+    </table>
+</body>
+</html>
+]], true))
+        end
+    elseif file_type == "file" then
+    
+        local fd, err, errno = io.open(real_path, "rb")
+        local code
+        if not fd then
+            if errno == ce.ENOENT then
+                code = "404"
+            elseif errno == ce.EACCES then
+                code = "403"
+            else
+                code = "503"
+            end
+            res_headers:upsert(":status", code)
+            res_headers:append("content-type", "text/plain")
+            assert(stream:write_headers(res_headers, req_method == "HEAD"))
+            if req_method ~= "HEAD" then
+                assert(stream:write_body_from_string("Fail!\n"..err.."\n"))
+            end
+        else
+            res_headers:upsert(":status", "200")
+            local mime_type = mdb and mdb:file(real_path) or "application/octet-stream"
+            res_headers:append("content-type", mime_type)
+            assert(stream:write_headers(res_headers, req_method == "HEAD"))
+            if req_method ~= "HEAD" then
+                assert(stream:write_body_from_file(fd))
+            end
+        end
+    elseif file_type == nil then
+    
+        res_headers:upsert(":status", "404")
+        print('three')
+        assert(stream:write_headers(res_headers, true))
+    else
+    print('four')
+        res_headers:upsert(":status", "403")
+        assert(stream:write_headers(res_headers, true))
+    end
+end
+
+local function websocket_reply(t, msg)
   
     if msg.cmd then
 
@@ -141,28 +352,21 @@ local function ProcessWebsocketMessage(t, msg)
         elseif cmd == "AUTH" then
 
         elseif cmd == "HELP" then
-	  write_to_process(msg.cmd..'\n')
+      write_to_process(msg.cmd..'\n')
         elseif cmd == "UNIT-RESPONSE" then
         else
             logger:info("Type=" .. msg.cmd)            
-	    write_to_process(msg.cmd..'\n')
+        write_to_process(msg.cmd..'\n')
         end
     end
 end
 
---- Get a UUID from the OS
--- return: Returns a system generated UUID
--- such as "4f1c1fbe-87a7-11e6-b146-0c54a518c15b"
-local function GetUUID()
-    local u = uuid()
-    print("here's a new uuid: ",u)
-    return u
-end
---- ProcessRequest is where we process the request from the client.
+
+--- process_request is where we process the request from the client.
 -- The system upgrades to a websocket if the ws or wss protocols are used.
 -- @param server ?
 -- @param An open stream to the client. Raw socket abstraction?
-local function ProcessRequest(server, stream)
+local function process_request(server, stream)
 
     local request_headers = assert(stream:get_headers())
     local request_method = request_headers:get ":method"
@@ -202,7 +406,7 @@ local function ProcessRequest(server, stream)
                     if DEBUG then
                         logger:info(serpent.block(msg))
                     end
-                    ProcessWebsocketMessage(t, msg)
+                    websocket_reply(t, msg)
                 else
                     logger:info("message could not be parsed")
                     logger:info(pos, err)
@@ -221,31 +425,35 @@ local function ProcessRequest(server, stream)
         logger:info("removed " .. id)
         Sessions[id] = nil
     else
-        --standard HTTP request. Need to still do something with it.
-        local request_content_type = request_headers:get("content-type")
-        local req_body = assert(stream:get_body_as_string(timeout))
-        logger:info(req_body)
-        local response_headers = http_headers.new()
-        response_headers:append(":status", "200")
-        response_headers:append("content-type", "text/plain")
-        response_headers:append("content-type", request_content_type  or "text/html")
-
-        assert(stream:write_headers(response_headers, request_method == "HEAD"))
-        -- Send headers to client; end the stream immediately if this was a HEAD request
-        if request_method == "HEAD" then return end;
-        -- Send body, ending the stream
-        local body = [[<html><head><head><body bgcolor="light blue">This server doesn't like http right now. Please use a websocket</body></html>]]
-        --resp:set_body([[<html><head><head><body bgcolor="light blue">This server doesn't like http right now. Please use a websocket</body></html>]])
-        assert(stream:write_chunk(body, true))
+        static_reply(server, stream, request_headers)
     end
 end
+    
+        --~ --standard HTTP request. Need to still do something with it.
+        --~ local request_content_type = request_headers:get("content-type")
+        --~ local req_body = assert(stream:get_body_as_string(timeout))
+        --~ logger:info(req_body)
+        --~ local response_headers = http_headers.new()
+        --~ response_headers:append(":status", "200")
+        --~ response_headers:append("content-type", "text/plain")
+        --~ response_headers:append("content-type", request_content_type  or "text/html")
+--~ 
+        --~ assert(stream:write_headers(response_headers, request_method == "HEAD"))
+        --~ -- Send headers to client; end the stream immediately if this was a HEAD request
+        --~ if request_method == "HEAD" then return end;
+        --~ -- Send body, ending the stream
+        --~ local body = [[<html><head><head><body bgcolor="light blue">This server doesn't like http right now. Please use a websocket</body></html>]]
+        --~ --resp:set_body([[<html><head><head><body bgcolor="light blue">This server doesn't like http right now. Please use a websocket</body></html>]])
+        --~ assert(stream:write_chunk(body, true))
+    --~ end
+
 
 local function Listen()
   local app_server = http_server.listen {
-		host = conf.host;
-		port = conf.port;
-		onstream = ProcessRequest;
-	}
+        host = conf.host;
+        port = conf.port;
+        onstream = process_request;
+    }
   -- Manually call :listen() so that we are bound before calling :localname()
   assert(app_server:listen())
   do
@@ -266,9 +474,9 @@ local function read_process(pty)
     local ok = pty:readok(0.1)
     if ok then
         data = pty:read(0.1)
-	for i,v in pairs(Sessions) do
-	  v.websocket:send(data)
-	end
+    for i,v in pairs(Sessions) do
+      v.websocket:send(data)
+    end
         print( '*', data, data:len() )
     else
       --Do nothing right now.
@@ -286,17 +494,17 @@ local function Run()
   local args = {'-Xmx1024M', '-Xms1024M', '-jar', '/home/russellh/Git/minecraft-runner/minecraft/minecraft_server.1.13.1.jar',  'nogui'}
 
   pty = lpty.new({raw_mode=true})
-  local ok = pty:startproc(command, table.unpack(args))
-  if not ok then 
-    print('bail') os.exit(-1) 
-  else 
-    if not pty:hasproc() then
-      print('no proc')
-    else
-      print('it should be running?')
-    end
-  end
-  
+  --~ local ok = pty:startproc(command, table.unpack(args))
+  --~ if not ok then 
+    --~ print('bail') os.exit(-1) 
+  --~ else 
+    --~ if not pty:hasproc() then
+      --~ print('no proc')
+    --~ else
+      --~ print('it should be running?')
+    --~ end
+  --~ end
+  --~ 
   cq = cqueues.new()
   cq:wrap(function() read_process(pty) end)
   cq:wrap(check_for_file)
@@ -307,7 +515,7 @@ local function Run()
 
   local cq_ok, err, errno = cq:loop()
   if not cq_ok then
-	  print(err, errno, "Jumped the loop.", debug.traceback())
+      print(err, errno, "Jumped the loop.", debug.traceback())
   end
   
 end
